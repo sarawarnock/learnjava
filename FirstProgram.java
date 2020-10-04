67b6928814fae560a2ab030ab70ad9e6903505c9
sarawarnock <sara@sarawarnock.com>
2020-10-04 12:03:55 -0600

    learning java

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,4 +1,6 @@
 //class must match file name exactly 
+
+//now doing this in IntelliJ IDEA
 public class FirstProgram {
     public static void main (String[] args) {
         System.out.println("Welcome to Java Programming!");
